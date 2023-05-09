6cd733ba8aeb49c4a9d19d83135d475d5f9b6687
a21liltr <a21liltr@student.his.se>
2023-05-09 15:40:58 +0200

    Minor fix: referred to wrong view id. Fixed.

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -9,12 +9,14 @@ import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView = findViewById(R.id.my_image);
+        imageView = findViewById(R.id.my_image);
         String imageURL = "https://plus.unsplash.com/premium_photo-1674035036840-02832578b956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80";
 
         // Using external library Picasso to load an image from URL into my Image View.
